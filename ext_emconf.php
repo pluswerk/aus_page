c120999fe8a688354401c94b8f32bd39980f6fad
Markus Hoelzle <m.hoelzle@andersundsehr.com>
2016-06-16 13:09:36 +0200

    [FEATURE] Initial commit

---
+++ ext_emconf.php
@@ -0,0 +1,22 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'anders und sehr: Page Extension',
+    'description' => '',
+    'category' => 'misc',
+    'author' => 'Markus Hölzle',
+    'author_email' => 'm.hoelzle@andersundsehr.com',
+    'state' => 'stable',
+    'internal' => '',
+    'uploadfolder' => '0',
+    'createDirs' => '',
+    'clearCacheOnLoad' => false,
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '7.6.9',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
